022281f383904dcf2788ea4220d353655ff2135a
Sa021di <bekmuradsaydaxmetov@gmail.com>
2025-01-08 19:21:22 +0500

    updated

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'admin-panel-cache-v1';
+const urlsToCache = [
+  '/admin.html',
+  '/assets/css/admin.css',
+  '/assets/js/script.js',
+  '/assets/icons/icon-192x192.png',
+  '/assets/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
